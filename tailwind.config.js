2f8f92b4ca20826b3d59229abdb8d5ff600c3aa1
Andre Lopez <andre.jl@yahoo.com>
2023-04-07 16:51:59 -0700

    Added necessary colors and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme")
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      "dark-grayish-blue": "hsl(236, 13%, 42%)",
+      "grayish-blue": "hsl(233, 8%, 79%)",
+      "off-white": "hsl(36, 100%, 99%)",
+      "soft-orange": "hsl(35, 77%, 62%)",
+      "soft-red": "hsl(5, 85%, 63%)",
+      "very-dark-blue": "hsl(240, 100%, 5%)",
+    },
+    extend: {
+      fontFamily: {
+        inter: ['"Inter"', ...defaultTheme.fontFamily.sans],
+      },
+    },
+    screens: {
+      xs: "375px",
+    },
   },
   plugins: [],
 }
