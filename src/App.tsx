a876de215081ec6f6f07375ef35446367f283b83
Andre Lopez <andre.jl@yahoo.com>
2023-04-12 22:24:21 -0700

    Corrected flexbox of sub articles at bottom

--- src/App.tsx
+++ src/App.tsx
@@ -38,7 +38,7 @@ function App() {
       )}
 
       {/* Nav bar */}
-      <nav className="flex flex-row justify-between mt-14 mb-6 items-center">
+      <nav className="flex flex-row justify-between mt-8 md:mt-14 mb-6 items-center">
         <img src="logo.svg" alt="news logo" />
         <ul className="hidden md:flex">
           <li className="font-inter font-medium mx-2 hover:text-soft-orange hover:cursor-pointer">Home</li>
@@ -67,8 +67,8 @@ function App() {
             <img src="image-web-3-mobile.jpg" alt="abstract main image" className="md:hidden" />
             <img src="image-web-3-desktop.jpg" alt="abstract main image" className="hidden md:block" />
 
-            <div className="flex flex-col md:flex-row align-top py-8 md:pr-6">
-              <h1 className="font-inter text-5xl font-bold md:mr-8 md:basis-1/2 md:pr-4">
+            <div className="flex flex-col md:flex-row align-top py-5 md:pr-6">
+              <h1 className="font-inter text-5xl font-bold md:mr-8 md:basis-1/2 mb-4">
                 The Bright Future of Web 3.0?
               </h1>
 
@@ -85,7 +85,7 @@ function App() {
           </div>
 
           {/* News Section */}
-          <article className="flex-col px-6 bg-very-dark-blue w-[500px] h-min pt-2 md:basis-1/3">
+          <article className="flex flex-col self-center px-6 bg-very-dark-blue h-min pt-2 md:basis-1/3 mt-8 md:mt-0">
             <h2 className="text-soft-orange font-bold text-3xl mt-4">New</h2>
             <div className="sub-article border-b-[1px] border-solid border-grayish-blue py-6">
               <h5 className="title font-inter text-off-white my-1 font-semibold text-lg hover:text-soft-orange hover:cursor-pointer">
@@ -115,11 +115,11 @@ function App() {
           </article>
         </div>
 
-        <div className="flex flex-col md:flex-row justify-between mt-4">
+        <div className="flex flex-col items-center md:items-start md:flex-row md:justify-between mt-10 mb-12">
           {/* Article 01 */}
-          <article className="flex w-[350px] mt-4 md:mt-0">
+          <article className="flex md:pr-4 my-2 max-w-md">
             <img src="image-retro-pcs.jpg" alt="retro pc" className="h-36 pr-4" />
-            <div className="flexbox-col">
+            <div className="flex flex-col">
               <h3 className="article-num font-inter font-bold text-2xl text-grayish-blue">01</h3>
               <h4 className="article-title font-inter my-2 font-bold hover:text-soft-red hover:cursor-pointer">
                 Reviving Retro PCs
@@ -131,9 +131,9 @@ function App() {
           </article>
 
           {/* Article 02 */}
-          <div className="flex w-[350px] mt-4 md:mt-0">
+          <article className="flex md:pr-4 my-2 max-w-md">
             <img src="image-top-laptops.jpg" alt="retro pc" className="h-36 pr-4" />
-            <div className="flexbox-col">
+            <div className="flex flex-col">
               <h3 className="article-num font-inter font-bold text-2xl text-grayish-blue">02</h3>
               <h4 className="article-title font-inter my-2 font-bold hover:text-soft-red hover:cursor-pointer">
                 Top 10 Laptops of 2022
@@ -142,12 +142,12 @@ function App() {
                 Our best picks for various needs and budgets.
               </h5>
             </div>
-          </div>
+          </article>
 
           {/* Article 03 */}
-          <div className="flex w-[350px] mt-4 md:mt-0 md:mb-4">
+          <article className="flex md:mb-4 my-2 max-w-md">
             <img src="image-gaming-growth.jpg" alt="retro pc" className="h-36 pr-4" />
-            <div className="flexbox-col">
+            <div className="flex flex-col">
               <h3 className="article-num font-inter font-bold text-2xl text-grayish-blue">03</h3>
               <h4 className="article-title font-inter my-2 font-bold hover:text-soft-red hover:cursor-pointer">
                 The Growth of Gaming
@@ -156,7 +156,7 @@ function App() {
                 How the pandemic has sparked fresh opportunities.
               </h5>
             </div>
-          </div>
+          </article>
         </div>
       </main>
     </div>
