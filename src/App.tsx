805f0c9679a9a76c14111122f8fc3d7aebb07917
Andre Lopez <andre.jl@yahoo.com>
2023-04-12 22:59:10 -0700

    Refactored classnames

--- src/App.tsx
+++ src/App.tsx
@@ -13,9 +13,9 @@ function App() {
       {/* Mobile Nav Menu */}
       {menuToggle && (
         <>
-          <div className="Blur absolute top-0 bottom-0 right-0 left-0 z-30 max-h-screen bg-dark-grayish-blue opacity-70"></div>
-          <div className="absolute top-0 bottom-0 right-0 z-40 w-2/3 sm:w-1/2 max-h-screen bg-off-white">
-            <button className="absolute top-10 right-5">
+          <div className="menu-blur absolute top-0 bottom-0 right-0 left-0 z-30 max-h-screen bg-dark-grayish-blue opacity-70"></div>
+          <div className="mobile-menu absolute top-0 bottom-0 right-0 z-40 w-2/3 sm:w-1/2 max-h-screen bg-off-white">
+            <button className="close-btn absolute top-10 right-5">
               <img
                 src="icon-menu-close.svg"
                 alt="menu toggle"
@@ -23,46 +23,32 @@ function App() {
                 onClick={toggleMenu}
               />
             </button>
-            <ul className="flex flex-col pl-4 pt-24 max-h-none">
-              <li className="font-inter text-xl mx-2 my-3 hover:text-soft-orange hover:cursor-pointer">
-                Home
-              </li>
-              <li className="font-inter text-xl mx-2 my-3 hover:text-soft-orange hover:cursor-pointer">
-                New
-              </li>
-              <li className="font-inter text-xl mx-2 my-3 hover:text-soft-orange hover:cursor-pointer">
-                Popular
-              </li>
-              <li className="font-inter text-xl mx-2 my-3 hover:text-soft-orange hover:cursor-pointer">
-                Trending
-              </li>
-              <li className="font-inter text-xl mx-2 my-3 hover:text-soft-orange hover:cursor-pointer">
-                Categories
-              </li>
+            <ul className="nav-list flex flex-col pl-4 pt-24 max-h-none">
+              <li className="nav-link-mobile">Home</li>
+              <li className="nav-link-mobile">New</li>
+              <li className="nav-link-mobile">Popular</li>
+              <li className="nav-link-mobile">Trending</li>
+              <li className="nav-link-mobile">Categories</li>
             </ul>
           </div>
         </>
       )}
 
       {/* Nav bar */}
-      <nav className="flex flex-row justify-between mt-8 md:mt-14 mb-6 items-center">
+      <nav className="nav-bar flex flex-row justify-between mt-8 md:mt-14 mb-6 items-center">
         <img src="logo.svg" alt="news logo" />
-        <ul className="hidden md:flex">
-          <li className="font-inter font-medium mx-2 hover:text-soft-orange hover:cursor-pointer">Home</li>
-          <li className="font-inter font-medium mx-2 hover:text-soft-orange hover:cursor-pointer">New</li>
-          <li className="font-inter font-medium mx-2 hover:text-soft-orange hover:cursor-pointer">Popular</li>
-          <li className="font-inter font-medium mx-2 hover:text-soft-orange hover:cursor-pointer">
-            Trending
-          </li>
-          <li className="font-inter font-medium mx-2 hover:text-soft-orange hover:cursor-pointer">
-            Categories
-          </li>
+        <ul className="nav-links hidden md:flex">
+          <li className="nav-link-desktop">Home</li>
+          <li className="nav-link-desktop">New</li>
+          <li className="nav-link-desktop">Popular</li>
+          <li className="nav-link-desktop">Trending</li>
+          <li className="nav-link-desktop">Categories</li>
         </ul>
         {!menuToggle && (
           <img
             src="icon-menu.svg"
             alt="menu toggle"
-            className="md:hidden hover:cursor-pointer"
+            className="hamburger-menu md:hidden hover:cursor-pointer"
             onClick={toggleMenu}
           />
         )}
@@ -92,29 +78,23 @@ function App() {
           </div>
 
           {/* News Section */}
-          <article className="flex flex-col self-center px-6 bg-very-dark-blue h-min pt-2 md:basis-1/3 mt-8 md:mt-0">
+          <article className="news-section flex flex-col self-center px-6 bg-very-dark-blue h-min pt-2 md:basis-1/3 mt-8 md:mt-0">
             <h2 className="text-soft-orange font-bold text-3xl mt-4">New</h2>
             <div className="sub-article border-b-[1px] border-solid border-grayish-blue py-6">
-              <h5 className="title font-inter text-off-white my-1 font-semibold text-lg hover:text-soft-orange hover:cursor-pointer">
-                Hydrogen VS Electric Cars
-              </h5>
+              <h5 className="news-article-title">Hydrogen VS Electric Cars</h5>
               <p className="info text-grayish-blue text-normal text-[15px] tracking-wider">
                 Will hydrogen-fueled cars ever catch up to EVs?
               </p>
             </div>
             <div className="sub-article border-b-[1px] border-solid border-grayish-blue py-6">
-              <h5 className="title font-inter text-off-white my-1 font-semibold text-lg hover:text-soft-orange hover:cursor-pointer">
-                The Downsides of AI Artistry
-              </h5>
+              <h5 className="news-article-title">The Downsides of AI Artistry</h5>
               <p className="info text-grayish-blue text-normal text-[15px] tracking-wider">
                 What are the possible adverse effects of on-demand AI image generation?
               </p>
               <hr />
             </div>
             <div className="sub-article py-6">
-              <h5 className="title font-inter text-off-white my-1 font-semibold text-lg hover:text-soft-orange hover:cursor-pointer">
-                Is VC Funding Drying Up?
-              </h5>
+              <h5 className="news-article-title">Is VC Funding Drying Up?</h5>
               <p className="info text-grayish-blue text-normal text-[15px] tracking-wider">
                 Private funding by VC firms is down 50% YOY. We take a look at what that means.
               </p>
@@ -122,15 +102,13 @@ function App() {
           </article>
         </div>
 
-        <div className="flex flex-col items-center md:items-start md:flex-row md:justify-between mt-10 mb-12">
+        <div className="numbered-articles flex flex-col items-center md:items-start md:flex-row md:justify-between mt-10 mb-12">
           {/* Article 01 */}
           <article className="flex md:pr-4 my-2 max-w-md">
             <img src="image-retro-pcs.jpg" alt="retro pc" className="h-36 pr-4" />
             <div className="flex flex-col">
               <h3 className="article-num font-inter font-bold text-2xl text-grayish-blue">01</h3>
-              <h4 className="article-title font-inter my-2 font-bold hover:text-soft-red hover:cursor-pointer">
-                Reviving Retro PCs
-              </h4>
+              <h4 className="article-title numbered-article-title">Reviving Retro PCs</h4>
               <h5 className="font-inter text-dark-grayish-blue font-semibold">
                 What happens when old PCs are given modern upgrades?
               </h5>
@@ -142,9 +120,7 @@ function App() {
             <img src="image-top-laptops.jpg" alt="retro pc" className="h-36 pr-4" />
             <div className="flex flex-col">
               <h3 className="article-num font-inter font-bold text-2xl text-grayish-blue">02</h3>
-              <h4 className="article-title font-inter my-2 font-bold hover:text-soft-red hover:cursor-pointer">
-                Top 10 Laptops of 2022
-              </h4>
+              <h4 className="article-title numbered-article-title">Top 10 Laptops of 2022</h4>
               <h5 className="font-inter text-dark-grayish-blue font-semibold">
                 Our best picks for various needs and budgets.
               </h5>
@@ -156,9 +132,7 @@ function App() {
             <img src="image-gaming-growth.jpg" alt="retro pc" className="h-36 pr-4" />
             <div className="flex flex-col">
               <h3 className="article-num font-inter font-bold text-2xl text-grayish-blue">03</h3>
-              <h4 className="article-title font-inter my-2 font-bold hover:text-soft-red hover:cursor-pointer">
-                The Growth of Gaming
-              </h4>
+              <h4 className="article-title numbered-article-title">The Growth of Gaming</h4>
               <h5 className="font-inter text-dark-grayish-blue font-semibold">
                 How the pandemic has sparked fresh opportunities.
               </h5>
