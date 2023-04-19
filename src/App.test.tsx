8a925369bad23fbb73b76a8a9a11b606a4c76553
Andre Lopez <andre.jl@yahoo.com>
2023-04-18 23:08:00 -0700

    Added a test for navbar...more to come

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,4 +1,19 @@
 import { describe, it } from "vitest"
 import { render, screen } from "@testing-library/react"
+import "@testing-library/jest-dom/extend-expect"
 
 import App from "./App"
+
+describe("App", () => {
+  it("Renders the navBar", () => {
+    // Arrange
+    render(<App />)
+
+    // Act
+
+    // Expect
+    const navItems = screen.getAllByRole("listitem")
+    const navTitles = navItems.map((item) => item.textContent)
+    expect(navTitles).toEqual(["Home", "New", "Popular", "Trending", "Categories"])
+  })
+})
